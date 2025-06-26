9420f1d70cc3b22435a817e1356128966b667e02
mustafailkerkamaci <mustafailkerkmc@gmail.com>
2025-06-27 02:17:33 +0300

    Add project files.

---
+++ BasicMVC_Practice/Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BasicMVC_Practice.Models;
+
+namespace BasicMVC_Practice.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john.doe@example.com"
+            };
+
+            var viewModel = new CustomerViewModel
+            {
+                Customer = customer,
+                WelcomeMessage = "Welcome to our customer portal!"
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ BasicMVC_Practice/Models/CustomerViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicMVC_Practice.Models
+{
+    public class CustomerViewModel
+    {
+
+        public Customer Customer { get; set; }
+        public string WelcomeMessage { get; set; } // Ekstra bir veri
+
+    }
+}
